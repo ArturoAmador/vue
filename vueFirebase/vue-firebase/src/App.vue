f641c0ff8062b0e959b8051c8639b962ce2246fa
Arturo Amador Paulino <amadorassassin@gmail.com>
2019-10-13 20:37:24 -0500

    init

---
+++ vueFirebase/vue-firebase/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="principal">
+    <span>APP</span>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+html{
+  font-size: 62.5% !important;
+}
+.principal{
+  background-color: red;
+  padding: 10px;
+  color: white;
+  border-radius: 5px;
+  font-size: 3rem;
+  text-align: center
+}
+
+</style>
